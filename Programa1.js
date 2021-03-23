a2666dc66195d98cbf23c825768f389bbb7fb766
jarom <jaromero.marz.234@gmail.com>
2021-03-23 10:15:05 -0700

    Modificando programa 1 para la clase auto

---
+++ Programa1.js
@@ -0,0 +1,17 @@
+class Auto {
+    constructor(marca, modelo, año, placas, color) {
+        marca = marca;
+        modelo.this;
+        año.this;
+        placas.this;
+        color.this;
+    }
+}
+
+let coche = new Auto;
+coche.color = rojo;
+coche.modelo = explorer;
+coche.año = 2003;
+
+console.log(coche);
+
---
+++ README.md
@@ -0,0 +1,3 @@
+Este es la nota para este repositorio
+En conclusion este solo es un repositorio de prueba
+en donde no importa la contribucion de otros usuarios
